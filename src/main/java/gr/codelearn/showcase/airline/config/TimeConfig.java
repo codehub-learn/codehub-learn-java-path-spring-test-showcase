ae8f2996c5d23e70d817cb74eba15fb918f87ecb
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2025-11-04 12:04:46 +0200

    Added some explanatory comments for TimeConfig.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- src/main/java/gr/codelearn/showcase/airline/config/TimeConfig.java
+++ src/main/java/gr/codelearn/showcase/airline/config/TimeConfig.java
@@ -7,6 +7,12 @@ import java.time.Clock;
 
 @Configuration
 public class TimeConfig {
+	/*
+	 * Problem -> Solution
+	 * Each service manually creates or receives a new clock -> One Spring bean provides a shared, injectable clock
+	 * Hard to override during testing -> Mock/Spy a single bean
+	 * Inconsistent “now” across services -> All time calculations share the same reference
+	 */
 	@Bean
 	Clock clock() {
 		return Clock.systemUTC();
